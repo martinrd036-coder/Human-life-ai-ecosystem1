fe9fbea539442341c0b96c4ced4db67d7bcd200d
martinrd036-coder <martinrd036@gmail.com>
2026-09-23 11:13:17 -0400

    Update SQL query to include additional values

--- server.js
+++ server.js
@@ -240,7 +240,7 @@ experimentPlan:buildExperimentPlan({
   testability_score,
   experiment_plan
      )
-     VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)
+     VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14))
      ON CONFLICT(id) DO NOTHING`,
     [
      x.id,
