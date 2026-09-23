ecb276714daafb5ebdf907a30c75a7c4d23ad7a2
martinrd036-coder <martinrd036@gmail.com>
2026-09-23 10:48:17 -0400

    Add more placeholders to SQL VALUES clause

--- server.js
+++ server.js
@@ -147,7 +147,7 @@ async function runLog(id,status,activity,result,start){
     started_at,
     completed_at
    )
-   VALUES($1,$2,$3,$4,$5,$6,$7)`,
+   VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)`,
   [
    `run-${Date.now()}-${Math.random().toString(36).slice(2,7)}`,
    id,
